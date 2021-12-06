3d438a2d555bdc6827e4004d84a1d5a2b9f849f2
Alex Boucher <aboucher51@gmail.com>
2021-12-06 15:36:05 -0500

    finished 3.1

--- 2021/03/main.go
+++ 2021/03/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"strconv"
 	"strings"
 )
 
@@ -22,14 +23,17 @@ func ReadFileAndParseToIntArray(fileName string) [][]string {
 
 	lines := strings.Split(string(file), "\n")
 	for _, line := range lines {
-		in = append(in, strings.Split(line, ""))
+		in = append(in, strings.Split(strings.TrimSpace(line), ""))
 	}
 
 	return in
 }
 
 func PartOne(input [][]string) int {
-	var myMapSlice []M
+	myMapSlice := make([]map[string]int, len(input[0]))
+	for i := 0; i < len(input[0]); i++ {
+		myMapSlice[i] = make(map[string]int)
+	}
 
 	for _, binaryString := range input {
 		for i := 0; i < len(binaryString); i++ {
@@ -37,7 +41,32 @@ func PartOne(input [][]string) int {
 		}
 	}
 
-	return 134324
+	var highFrequencies string
+	var lowFrequencies string
+
+	for i := 0; i < len(input[0]); i++ {
+		//log.Printf("1: %d, 0: %d\n", myMapSlice[i]["1"], myMapSlice[i]["0"])
+		if myMapSlice[i]["1"] > myMapSlice[i]["0"] {
+			highFrequencies += "1"
+			lowFrequencies += "0"
+		} else {
+			highFrequencies += "0"
+			lowFrequencies += "1"
+		}
+	}
+
+	log.Println(highFrequencies)
+	log.Println(lowFrequencies)
+	gammaRate, _ := strconv.ParseInt(highFrequencies, 2, 64)
+	epsilonRate, _ := strconv.ParseInt(lowFrequencies, 2, 64)
+	log.Println(gammaRate)
+	log.Println(epsilonRate)
+	return int(gammaRate) * int(epsilonRate)
+}
+
+func Atoi(s string) int {
+	i, _ := strconv.Atoi(strings.TrimSpace(s))
+	return i
 }
 
 type M map[int]interface{}
