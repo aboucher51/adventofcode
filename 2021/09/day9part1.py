13d8d97766ddb910b379572d73d66680c49dcc0e
Alex Boucher <aboucher51@gmail.com>
2021-12-09 16:13:23 -0500

    started day 9. saucing it up with a little python

---
+++ 2021/09/day9part1.py
@@ -0,0 +1,17 @@
+heightmap = {}
+with open("input.txt") as file:
+    for line in file:
+        heightmap.append(list(line.rstrip()))
+
+localMinima = {}
+
+for i in range(len(heightmap)):
+    for j in range(len(heightmap[i])):
+        currentPoint = heightmap[i][j]
+        if(    (i==0 or (heightmap[i-1][j] > currentPoint))
+           and (j==0 or (heightmap[i][j-1] > currentPoint))
+           and (i==len(heightmap)-1 or (heightmap[i+1][j] > currentPoint))
+           and (j==len(heightmap[i])-1 or (heightmap[i][j+1] > currentPoint))):
+            localMinima.append(currentPoint)
+
+print(localMinima)
